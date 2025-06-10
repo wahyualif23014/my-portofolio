99240902bcd1dccef85f952c17aaea7d1e0c030c
wahyualif23014 <zexvulca@gmail.com>
2025-06-10 17:55:00 +0700

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ const Footer = () => {
             <div className="link-section">
               <h4>Navigation</h4>
               <ul>
-                <li><a href="#home">Home</a></li>
+                <li><a href="#hero">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
