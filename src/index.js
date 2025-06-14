1f39a02ff4578d6500eca7ac2904fb68ca0d4e62
wahyualif23014 <zexvulca@gmail.com>
2025-06-14 13:50:30 +0700

    nde

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // pastikan import CSS
+import './index.css';  
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
